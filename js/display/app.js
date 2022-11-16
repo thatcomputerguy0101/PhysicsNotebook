ae7a82e6f6e0b035ac03736b6c991d0d82cbbe72
amedawg <83100446+amedawg@users.noreply.github.com>
2022-11-15 21:31:24 -0500

    worked on design of web app

--- js/display/app.js
+++ js/display/app.js
@@ -6,9 +6,24 @@ export class App extends React.Component {
     return rhtml`
     <div className="app">
       <header></header>
-      <aside></aside>
-      <main></main>
-      <aside></aside>
+      <aside>
+        <header>
+          <img src="/icons/placeholder.svg"/>
+          <div> Givens </div>
+          <img src="/icons/placeholder.svg"/>
+        </header>
+      </aside>
+      <main>
+        <section></section>
+        <footer></footer>
+      </main>
+      <aside>
+        <header>
+          <img src="/icons/placeholder.svg"/>
+          <div> Problems </div>
+          <img src="/icons/placeholder.svg"/>
+        </header>
+      </aside>
     </div>`
   }
 }
