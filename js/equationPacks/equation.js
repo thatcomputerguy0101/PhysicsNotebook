decb454a251fd41e0953ec441b38e52b5d9679c8
thatcomputerguy0101 <thatcomputerguy0101@gmail.com>
2022-12-05 10:37:17 -0500

    Add selection api to Equation

--- js/equationPacks/equation.js
+++ js/equationPacks/equation.js
@@ -1,14 +1,54 @@
 import html from "../html.js"
 
 export class Equation extends React.Component {
+  static defaultProps = {
+    editable: false,
+    onEnter: () => {},
+    onEdit: () => {},
+    onSelectionChange: () => {},
+    onMoveOutOf: () => {},
+    onSelectOutOf: () => {},
+    onUpOutOf: () => {},
+    onDownOutOf: () => {}
+  }
+
   constructor(...args) {
     super(...args)
     this.mqRef = React.createRef()
+    this.handleSelection = this.handleSelection.bind(this)
   }
 
   componentDidMount() {
+    document.addEventListener("selectionchange", this.handleSelection)
     // When added to the visible structure
-    MQ.StaticMath(this.mqRef.current)
+    if (!this.props.editable) {
+      this.math = MQ.StaticMath(this.mqRef.current)
+      // this.registerStaticHandlers()
+      if (this.math.innerFields.length > 0) {
+        // for field in fields this.registerDynamicHandlers()
+      }
+    } else {
+      this.math = MQ.MathField(this.mqRef.current, {
+        handlers: {
+          enter: this.props.onEnter,
+          edit: this.props.onEdit,
+          moveOutOf: this.props.onMoveOutOf,
+          selectOutOf: this.props.onSelectOutOf,
+          upOutOf: this.props.onUpOutOf,
+          downOutOf: this.props.onDownOutOf
+        }
+      })
+    }
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("selectionchange", this.handleSelection)
+  }
+
+  handleSelection(event) {
+    if (this.mqRef.current.contains(document.getSelection().focusNode) && typeof this.props.onSelectionChange == "function") {
+      this.props.onSelectionChange(this.math.getSelection())
+    }
   }
 
   render() {
--- js/workbooks/equationState.js
+++ js/workbooks/equationState.js
@@ -2,11 +2,22 @@ import html from "../html.js"
 import { Equation } from "../equationPacks/equation.js"
 
 export class EquationState extends React.Component {
+  constructor(...args) {
+    super(...args)
+    this.handleSelection = this.handleSelection.bind(this)
+  }
+
   render() {
     let rhtml = html.bind({Equation})
     return rhtml`
-    <Equation>
+    <Equation onSelectionChange=${this.handleSelection}>
       ${this.props.value}
     </Equation>`
+  }
+
+  handleSelection(selection) {
+    // Check what manupulations are valid based on the selection here (maybe with a slight delay)
+    // Then allow the user to choose one to apply, which creates a new EquationState
+    console.log("New selection:", selection)
   }
 }
