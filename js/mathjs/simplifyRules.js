4e32459b7d308fff2544c1fef201090cdb596ee4
exb5429 <exb5429@psu.edu>
2022-12-06 15:55:27 -0500

    Fixed Simplify Rules

--- js/mathjs/simplifyRules.js
+++ js/mathjs/simplifyRules.js
@@ -1,13 +1,13 @@
 
 export default function modifyRules(mjs) {
-  const startIndex = mjs.simplify.rules.indexOf(rule =>
+  const startIndex = mjs.simplify.rules.findIndex(rule =>
     rule.s == "n*(n1/n2) -> (n*n1)/n2" ||
     rule.l == "n*(n1/n2)" && rule.r == "(n*n1)/n2"
   )
 
   if (startIndex == -1) {
     // Rule not found
-    throw new ValueError("MathJS changed its simplify rules, and the fraction rule was unable to be located")
+    throw new Error("MathJS changed its simplify rules, and the fraction rule was unable to be located")
   }
 
   mjs.simplify.rules.splice(
