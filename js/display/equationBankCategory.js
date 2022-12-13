9150970f1e0a4a3b448e09928be3f95cc230ee28
amedawg <83100446+amedawg@users.noreply.github.com>
2022-12-12 20:56:07 -0500

    Added icon for dropdown

--- js/display/equationBankCategory.js
+++ js/display/equationBankCategory.js
@@ -9,6 +9,7 @@ export class EquationBankCategory extends React.Component {
       <div>
         <div className=${"categoryHeader" + (this.props.open ? " open" : "")} onClick=${this.props.onClick}>
           ${this.props.name}
+          <img src="../icons/dropdown.svg" width = 20px height = 20px/>
         </div>
         ${
           this.props.open
