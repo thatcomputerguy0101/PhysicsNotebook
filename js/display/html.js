c6fd7dd0d9eb3c56e1d56b4df8af23579146e110
thatcomputerguy0101 <thatcomputerguy0101@gmail.com>
2024-06-24 16:57:33 -0400

    Add documentation and type annotations to React html parser

--- js/display/html.js
+++ js/display/html.js
@@ -1,13 +1,19 @@
-
-let Fragment = React.Fragment
+// // @ts-check
+// import React from "react"
 
 /**
   * Parses HTML fragments with possible insertions to React syntax. Insertions can be either text or an externally parsed node
   * This acts as a raw string annotation, demonstrated as follows: html`<tag><subtag/></tag>`
   *
   * When zero or multiple root-level elements are provided, this combines them together as a React.Fragment
+  * @param {TemplateStringsArray} source
+  * @param {...React.ReactNode} subsitutions
+  * @returns {React.ReactNode}
   */
 export default function html(source, ...subsitutions) {
+  /**
+   * @type {{string: React.ComponentType}}
+   */
   let bindings = {}
   if (typeof this == "object") {
     bindings = this
@@ -19,12 +25,19 @@ export default function html(source, ...subsitutions) {
     throw new ParsingError("Unexpected input at end")
   }
   if (Array.isArray(result)) {
-    result = React.createElement(Fragment, null, ...result)
+    result = React.createElement(React.Fragment, null, ...result)
   }
   return result
 }
 
+/**
+ * Error thrown whenever something unexpected occurs during HTML parsing
+ */
 class ParsingError extends SyntaxError {
+  /**
+   * Creates a new ParsingError
+   * @param {string} message Message describing what when wrong during parsing
+   */
   constructor(message) {
     super(message)
     this.name = "ParsingError"
@@ -40,23 +53,32 @@ let decodeEntities = (function() {
   // Cache this element so that it is only created once
   const element = document.createElement('div');
 
-  return function decodeEntities (str) {
-    if (str && typeof str === 'string') {
-      // Remove script/html tags
-      str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '')
-      str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '')
-      // Put sanatized input in an HTML element as source code
-      element.innerHTML = str
-      // Read it back out as text
-      str = element.textContent
-      // Reset the element
-      element.textContent = ''
-    }
-
+  /**
+   * Converts HTML ampresand entities to their corresponding characters
+   * @param {string} str Raw HTML string
+   * @returns {string} Parsed string
+   */
+  return function decodeEntities(str) {
+    // Remove script/html tags
+    str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '')
+    str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '')
+    // Put sanatized input in an HTML element as source code
+    element.innerHTML = str
+    // Read it back out as text
+    // @ts-ignore
+    str = element.textContent
+    // Reset the element
+    element.textContent = ''
     return str
   }
 })()
 
+/**
+ * Parses a sequence of HTML tags, comments, and body text
+ * @param {HTMLTokenizer} tokenizer HTML token generator
+ * @returns {React.ReactNode}
+ * @throws {ParsingError}
+ */
 function fragment(tokenizer) {
   if (tokenizer.isEmpty()) {
     return []
@@ -122,6 +144,12 @@ function fragment(tokenizer) {
   }
 }
 
+/**
+ * Parses a HTML tag
+ * @param {HTMLTokenizer} tokenizer HTML token generator
+ * @returns {React.ReactElement}
+ * @throws {ParsingError}
+ */
 function tag(tokenizer) {
   let start = tokenizer.nextToken()
   if (start.type != Token.Type.tag_start) {
@@ -139,12 +167,12 @@ function tag(tokenizer) {
     let attribute = attrOrEnd
     attrOrEnd.consume()
     attrOrEnd = tokenizer.nextToken()
-    let value = true
+    let value
     if (attrOrEnd.type == Token.Type.attribute_assign) {
       attrOrEnd.consume()
       value = tokenizer.nextToken()
       if (value.type != Token.Type.attribute_value) {
-        throw new ParsingError(`Expected an attribute value, got ${value.type} instead`)
+        throw new ParsingError(`Expected an attribute value, got ${Token.Type.key(value.type)} instead`)
       }
       value = value.consume().data
       attrOrEnd = tokenizer.nextToken()
@@ -184,6 +212,11 @@ function tag(tokenizer) {
   }
 }
 
+/**
+ * Parses a HTML comment
+ * @param {HTMLTokenizer} tokenizer HTML token generator
+ * @throws {ParsingError}
+ */
 function comment(tokenizer) {
   // This currently just verifies correct comment syntax and discards the results
   let start = tokenizer.nextToken()
@@ -203,20 +236,41 @@ function comment(tokenizer) {
   bodyOrEnd.consume()
 }
 
+/**
+ * Parses HTML tokens from template string information
+ */
 export class HTMLTokenizer {
+  /**
+   * Creates a new tokenizer
+   * @param {string[]} source
+   * @param {React.ReactNode[]} subsitutions
+   * @param {{string: React.ComponentType}} bindings
+   */
   constructor(source, subsitutions, bindings) {
     this.source = source
     this.subsitutions = subsitutions
     this.bindings = bindings
     // Initial state machine state
-    this.activeToken = new Token(Token.Type.text, null)
+    /**
+     * @type {Token}
+     */
+    this.activeToken = new Token(Token.Type.text, undefined)
     this.activeToken.consume()
   }
 
+  /**
+   * Checks if there is an available token to parse
+   * @returns {boolean}
+   */
   isEmpty() {
     return this.source.length == 0 || this.source.length == 1 && this.source[0].length == 0
   }
 
+  /**
+   * Gets the next non-consumed HTML token, parsing if required
+   * @returns {Token} Class representing the token type and associated literal
+   * @throws {ParsingError}
+   */
   nextToken() {
     if (!this.activeToken.consumed) {
       return this.activeToken
@@ -230,6 +284,7 @@ export class HTMLTokenizer {
 
     if ([Token.Type.tag_end, Token.Type.comment_end, Token.Type.text].includes(this.activeToken.type)) {
       // Not in a tag, can start a new tag or can parse text
+      let match
       if (this.source[0].match(/^<!--/)) {
         this.activeToken = new Token(Token.Type.comment_start)
         this.source[0] = this.source[0].slice(4)
@@ -241,9 +296,9 @@ export class HTMLTokenizer {
         // Text (or external tag) from subsitution
         this.source.splice(0, 1) // Remove empty string from source
         this.activeToken = new Token(Token.Type.text, this.subsitutions.splice(0, 1)[0])
-      } else if (this.source[0].match(/[^<>]+/) != null) {
+      } else if ((match = this.source[0].match(/[^<>]+/)) != null) {
         // Text from source
-        let data = RegExp.lastMatch
+        let data = match[0]
         this.activeToken = new Token(Token.Type.text, decodeEntities(data))
         this.source[0] = this.source[0].slice(this.activeToken.data.length)
       }
@@ -258,7 +313,7 @@ export class HTMLTokenizer {
       // At the start of a tag
       if (this.source[0].match(/^\/?>/) != null) {
         // If an empty tag, return a fragment
-        this.activeToken = new Token(Token.Type.tag_name, Fragment)
+        this.activeToken = new Token(Token.Type.tag_name, React.Fragment)
       } else {
         // Parse the name of the tag
         this.activeToken = new Token(Token.Type.tag_name, this.matchName())
@@ -289,31 +344,34 @@ export class HTMLTokenizer {
     return this.activeToken
   }
 
+  /**
+   * Removes extra whitespace from the start of the source string
+   */
   stripWhitespace() {
-    // Remove extra whitespace from the start of the source string
     this.source[0] = this.source[0].trimStart()
   }
 
+  /**
+   * HTML tag or attribute name
+   * @returns {(string|boolean|number)} Matched body token
+   * @throws {ParsingError}
+   */
   matchName() {
     // HTML tag or attribute name
-    let tag_name
-    if (this.source[0].length > 1) {
+    let tag_name = this.matchOrSubsitute(() => {
       // Pull from source
-      let match = this.source[0].match(/^[A-Za-z][A-Za-z0-9\-_:.]+/)
+      let match = this.source[0].match(/^[A-Za-z][A-Za-z0-9\-_:.]*/)
       if (match == null) {
-        if (this.activeToken.type == Token.Type.tag_open) {
+        if (this.activeToken.type == Token.Type.tag_start) {
           throw new ParsingError(`Expected a tag name following ${this.activeToken.closing ? "</" : "<"}`)
         } else {
           throw new ParsingError("Expected an attribute name")
         }
       }
-      tag_name = match[0]
+      let tag_name = match[0]
       this.source[0] = this.source[0].slice(tag_name.length)
-    } else {
-      // Insert subsitution
-      this.source.splice(0, 1) // Remove empty string from source
-      tag_name = this.subsitutions.splice(0, 1)[0]
-    }
+      return tag_name
+    })
     if (tag_name in this.bindings) {
       // Bindings represent subsitutions of names to special element types
       tag_name = this.bindings[tag_name]
@@ -321,12 +379,17 @@ export class HTMLTokenizer {
     return tag_name
   }
 
+  /**
+   * HTML attribute value, quoted or unquoted.
+   * Unquoted values may be parsed as booleans or numbers.
+   * @returns {(string|boolean|number)} Matched body token
+   * @throws {ParsingError}
+   */
   matchString() {
-    // HTML attribute value, quoted or unquoted
-    let attribute_value
-    if (this.source[0].length > 1) {
+    return this.matchOrSubsitute(() => {
       // Pull from source
       let match
+      let attribute_value
       if ((match = this.source[0].match(/^(?:"([^"<]*)"|'([^'<]*)')/)) != null) {
         attribute_value = match[1] ?? match[2]
       } else if ((match = this.source[0].match(/^[A-Za-z0-9\-_]+/)) != null) {
@@ -344,47 +407,88 @@ export class HTMLTokenizer {
         throw new ParsingError("Expected an attribute value")
       }
       this.source[0] = this.source[0].slice(match[0].length)
-      attribute_value = decodeEntities(attribute_value)
-    } else {
-      // Insert subsitution
-      this.source.splice(0, 1) // Remove empty string from source
-      attribute_value = this.subsitutions.splice(0, 1)[0]
-    }
-    return attribute_value
+      if (typeof attribute_value == "string") {
+        return decodeEntities(attribute_value)
+      } else {
+        return attribute_value
+      }
+    })
   }
 
+  /**
+   * Comment body text
+   * @returns {string} Matched body text
+   * @throws {ParsingError}
+   */
   matchComment() {
-    let text
-    if (this.source[0].length > 0) {
+    return this.matchOrSubsitute(() => {
       let match = this.source[0].match(/^(?:[^-]|-[^-]|--[^>])+/)
       if (match == null) {
         throw new ParsingError("Encountered unexpected comment end")
       }
-      text = match[0]
+      let text = match[0]
       this.source[0] = this.source[0].slice(text.length)
+      return text
+    })
+  }
+
+  /**
+   * For parsing tokens that support subsitution.
+   * Pulls from source with matcher if available, pulls directly from a subsitution otherwise.
+   * @template {React.ReactNode} Match
+   * @param {() => Match} matcher - A parameterless function to parse a valid token from source
+   * @returns {Match} Matched token literal (usually a string)
+   */
+   matchOrSubsitute(matcher) {
+    if (this.source[0].length > 0) {
+      return matcher()
     } else {
+      // Insert subsitution
       this.source.splice(0, 1) // Remove empty string from source
-      text = this.subsitutions.splice(0, 1)[0]
+      // @ts-ignore
+      return this.subsitutions.splice(0, 1)[0]
     }
-    return text
   }
 }
 
-
+/**
+ * Class representing a single HTML token, including type and data
+ * @template {React.ReactNode | React.ComponentType} Data
+ */
 class Token {
+  /**
+   * Enumeration representing a HTML token type
+   * @readonly
+   * @enum {number}
+   */
   static Type = class Type {
+    /** Literal text, not part of a tag */
     static text = 0
+    /** The opening symbols of a tag (either opening or closing tag) */
     static tag_start = 1
+    /** The name of a tag */
     static tag_name = 2
+    /** The closing symbols of a tag (either regular or self-closing tag) */
     static tag_end = 3
+    /** The name of a tag attribute */
     static attribute_name = 4
+    /** The assignment symbol of an attribute with a value */
     static attribute_assign = 5
+    /** The value of an attribute */
     static attribute_value = 6
+    /** The opening symbols of a comment tag */
     static comment_start = 7
+    /** The content of a comment tag */
     static comment_body = 8
+    /** The closing symbols of a comment tag */
     static comment_end = 9
 
-    static name(self) {
+    /**
+     * Converts from the integer representation of a HTML token type back to its name
+     * @param {Type} self Integer representing the token type
+     * @returns {string} Name of the token type
+     */
+    static key(self) {
       switch (self) {
         case 0:
           return "text"
@@ -406,24 +510,48 @@ class Token {
           return "comment_body"
         case 9:
           return "comment_end"
+        default:
+          return "unknown"
       }
     }
   }
 
-  constructor(type, data, text) {
+  /**
+   * Creates a new HTML token
+   * @param {Type} type Integer representing the token type
+   * @param {Data} [data] Additional data, such as name or presence of a closing slash
+   */
+  constructor(type, data) {
+    /** @type {Type} */
     this.type = type
+    /** @type {Data | undefined} */
     this.data = data
+    /** @type {boolean} */
     this.consumed = false
   }
 
+  /**
+   * Name of a tag or attribute
+   * @type {string}
+   */
   get name() {
+    // @ts-ignore
     return this.data
   }
 
+  /**
+   * True if this token includes a closing slash
+   * @type {boolean}
+   */
   get closing() {
+    // @ts-ignore
     return this.data
   }
 
+  /**
+   * Marks a token as consumed and that the parser is ready for the next token
+   * @returns {Token} This object for shorthand chaining
+   */
   consume() {
     this.consumed = true
     return this
